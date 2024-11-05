1bb6842ab2527b0e17ba128424cd0972124e5593
Sravaniganthala <146628579+Sravaniganthala@users.noreply.github.com>
2024-11-05 18:15:17 +0530

    Add files via upload

---
+++ jsday3.js
@@ -0,0 +1,26 @@
+//var a=10;//
+//console.log(a);
+//{}//block user:where is engine is expected to have single line,
+//there we can use multiple line
+
+var a;//declare
+var a;//re-declare
+a=10;//assign
+a=20;//re-assign
+var b=30; 
+var b="ravi"; //re initialize
+//let
+let a;
+let a1;//re-declare
+ a1=10;
+ a1=20;//re-assign
+ let x=30;// initialisation
+ //let x=40;  re initialize is not access
+ //const
+
+ //const b;  no declare
+ //const b1;
+ b1=10;
+ b1=20;
+ const c=30;
+ //const c=40; no re initialization
